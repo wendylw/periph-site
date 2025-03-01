f35ad6db921ce590f33f046e1d570f60bba4d3ba
wendylw <wendylw2013@gmail.com>
2025-03-01 20:51:01 +0800

    Update header

--- src/containers/home/index.jsx
+++ src/containers/home/index.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-import { View, Text } from "@tarojs/components";
+import { View } from "@tarojs/components";
 import Header from "@common/components/Header";
 
 function Home() {
   return (
     <View>
       <Header />
-      <Text>Hello world!</Text>
     </View>
   );
 }
