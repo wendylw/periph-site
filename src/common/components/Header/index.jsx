53ab7e67c749955f0e6bf940c191dfb5758774ee
Wendy Lin <wendylw2013@gmail.com>
2025-03-09 10:52:37 +0800

    Update login default button in header

--- src/common/components/Header/index.jsx
+++ src/common/components/Header/index.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { View, Image, Navigator, Button } from "@tarojs/components";
+import { UserCircle } from "phosphor-react";
 import { getClassName } from "@common/utils/ui";
 import styles from "./index.module.scss";
 
-const Header = ({ navigators = [] }) => {
+const Header = ({ navigators = [], onLoginButtonClick = () => {} }) => {
   const headerContentClassName = getClassName([
     styles.HeaderContent,
     "tw-flex tw-justify-between tw-items-center tw-mx-auto",
@@ -12,10 +13,6 @@ const Header = ({ navigators = [] }) => {
   const headerLogoFigureClassName = getClassName([
     styles.HeaderLogoFigure,
     "tw-flex tw-items-center tw-px-16 sm:tw-px-16px tw-h-full",
-  ]);
-  const headerNavigatorClassName = getClassName([
-    styles.HeaderNavigator,
-    "tw-flex tw-justify-center tw-px-16 sm:tw-px-16px tw-items-center tw-text-base tw-h-full",
   ]);
 
   return (
@@ -30,23 +27,29 @@ const Header = ({ navigators = [] }) => {
           />
         </figure>
 
-        <nav className="tw-flex tw-items-center tw-h-full">
-          {navigators.map((navigator) => (
-            <Navigator
-              key={navigator.key}
-              className={headerNavigatorClassName}
-              url={navigator.url}
-            >
-              {navigator.label}
-            </Navigator>
-          ))}
-        </nav>
+        <div className="tw-flex tw-items-center">
+          <nav className="tw-flex tw-items-center tw-h-full">
+            {navigators.map((navigator) => (
+              <Navigator
+                key={navigator.key}
+                className="tw-flex tw-justify-center tw-px-16 sm:tw-px-16px tw-items-center tw-text-base tw-h-full"
+                url={navigator.url}
+              >
+                {navigator.label}
+              </Navigator>
+            ))}
+          </nav>
 
-        <View className="login-container">
-          <Button className="login-button" onClick={() => {}}>
-            登录
-          </Button>
-        </View>
+          <View>
+            <Button
+              className={styles.HeaderLoginButton}
+              plain
+              onClick={onLoginButtonClick}
+            >
+              <UserCircle size={24} weight="light" />
+            </Button>
+          </View>
+        </div>
       </div>
     </View>
   );
